c4059d974ef5fa61d9177f435a96f7a1ab2990c4
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-05-11 00:11:14 +0500

    Write markdown for a list item

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import Header from "./Header.svelte"
 	import NewListItem from "./NewListItem.svelte"
+	import ListItem from "./ListItem.svelte"
 </script>
 
 <Header />
 
 <main class="main">
 	<NewListItem />
+
+	<ul class="media-list">
+		<ListItem title="Cyberpunk: Edgerinners" id="testItem" tag="Movie" />
+	</ul>
 </main>
---
+++ src/routes/ListItem.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import Fa from "svelte-fa"
+
+	import { faXmark } from "@fortawesome/free-solid-svg-icons"
+	import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
+	import { faThumbTack } from "@fortawesome/free-solid-svg-icons"
+
+	export let title: string
+	export let id: string
+	export let tag: string
+	export let checked = false
+	export let pinned = false
+</script>
+
+<li class="media-item">
+	<label for={id} class="media-item__label">
+		{title}
+		<span class="media-item__checkbox" />
+	</label>
+
+	<input type="checkbox" class="hidden" {id} name={title} bind:checked />
+
+	<button class="button-icon button-icon--delete">
+		<Fa icon={faXmark} />
+	</button>
+
+	<p class="media-item__tag">{tag}</p>
+
+	<div class="media-item__controls">
+		<button class="button-icon">
+			<span class="sr-only">Edit</span>
+			<Fa icon={faPenToSquare} />
+		</button>
+
+		<button class="button-icon">
+			<span class="sr-only">Pin</span>
+			<Fa icon={faThumbTack} />
+		</button>
+	</div>
+</li>
