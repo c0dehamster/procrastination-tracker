8384558093a0179ec878074ba996846d9a75ae68
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-05-10 20:34:27 +0500

    Populate header

---
+++ src/routes/Header.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import ToggleTags from "./ToggleTags.svelte"
+	import TagCheckbox from "./TagCheckbox.svelte"
+
+	let tagListExpanded = false
+	let tags = ["Movie", "Book", "Article", "Tutorial", "Project"]
+	let tagsActive: Array<string> = []
+</script>
+
+<header class="header">
+	<ToggleTags checked={tagListExpanded} />
+
+	<h1 class="logo">
+		<span class="logo__section">Procrasti</span>
+		<span class="logo__section">nation</span>
+	</h1>
+
+	<fieldset class="tags">
+		<legend class="sr-only">Tags</legend>
+
+		<ul class="tags__list">
+			{#each tags as name}
+				<TagCheckbox {name} bind:group={tagsActive} />
+			{/each}
+		</ul>
+	</fieldset>
+</header>
---
+++ src/routes/ToggleTags.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Fa from "svelte-fa"
+	import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
+
+	export let checked = false
+</script>
+
+<div class="tags">
+	<label for="" class="tags__label">
+		Tags
+		<span class="tags__icon">
+			<Fa icon={faArrowRight} />
+		</span>
+	</label>
+
+	<input type="checkbox" class="hidden" bind:checked />
+</div>
